3c19e37482feee37d459ae205ef0ea70a98b69e8
VIctor Boy Simamora <17241801+quedeve@users.noreply.github.com>
2022-09-07 07:06:35 +0700

    Initial Commit

---
+++ controllers/bootcamps.js
@@ -0,0 +1,58 @@
+const ErrorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middleware/async')
+const Bootcamp = require('../models/Bootcamp');
+
+// @desc    Get all bootcamps
+// @route   Get /api/v1/bootcamps
+// @access  Public
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({ success: true, data: bootcamps, count:bootcamps.length });
+});
+
+// @desc    Get single bootcamps
+// @route   Get /api/v1/bootcamps/:id
+// @access  Public
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+  
+    const bootcamp = await Bootcamp.findById(req.params.id);;
+    if(!bootcamp){
+      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+    }
+    res.status(200).json({ success: true, data: bootcamp });
+  
+
+});
+
+// @desc    Crete new bootcamps
+// @route   Create /api/v1/bootcamps
+// @access  Private
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({ success: true, data: bootcamp });
+});
+
+// @desc    Update bootcamp
+// @route   Update /api/v1/bootcamps/:id
+// @access  Private
+exports.updateBootcamp = asyncHandler(async(req, res, next) => {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body,{
+      new: true,
+      runValidators: true})
+      if(!bootcamp){
+        return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+      }
+      res.status(200).json({ success: true, data: bootcamp});
+});
+
+// @desc    Delete bootcamp
+// @route Delete /api/v1/bootcamps/:id
+// @access  Private
+exports.deleteBootcamp = asyncHandler(async(req, res, next) => {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    if(!bootcamp){
+      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+    }
+    res.status(200).json({ success: true, data: bootcamp});
+  
+});
---
+++ models/Bootcamp.js
@@ -0,0 +1,108 @@
+const mongoose = require("mongoose");
+const slugify = require("slugify");
+
+const BootcampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+    unique: true,
+    trim: true,
+    maxlength: [50, "Name can not be more than 50 characters"],
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, "Please add a description"],
+    trim: true,
+    maxlength: [500, "Description can not be more than 50 characters"],
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      "Please use a valid URL with HTTP or HTTPS",
+    ],
+  },
+  phone: {
+    type: String,
+    maxlength: [20, "Phone number can not be more than 20 characters"],
+  },
+  email: {
+    type: String,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please enter a valid email address",
+    ],
+  },
+  address: {
+    type: String,
+    required: [true, "Please add an address"],
+  },
+  location: {
+    //GeJSON Point -- mongoose getJson
+    type: {
+      type: String,
+      enums: ["POINT"],
+    },
+    coordinates: {
+      type: [Number],      
+      index: "2dsphere",
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
+  careers: {
+    type: [String],
+    required: true,
+    enum: [
+      "Web Development",
+      "Mobile Development",
+      "UI/UX",
+      "Data Science",
+      "Business",
+      "Other",
+    ],
+  },
+  averageRating: {
+    type: Number,
+    min: [1, "Rating must be at least 1"],
+    max: [10, "Rating must can not be more than 10"],
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: "no-photo.jpg",
+  },
+  housing: {
+    type: Boolean,
+    default: false,
+  },
+  jobAssistance: {
+    type: Boolean,
+    default: false,
+  },
+  jobGuarantee: {
+    type: Boolean,
+    default: false,
+  },
+  acceptGi: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre('save', function(next){
+  this.slug = slugify(this.name, {lower:true})
+  next();
+})
+
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
